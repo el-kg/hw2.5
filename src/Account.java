e91eaef3654c2a1b8243676cdcb319d9a7b244d1
el-kg <kirillgalkin8@gmail.com>
2023-07-30 17:20:49 +0700

    Внесены правки

--- src/Account.java
+++ src/Account.java
@@ -1,27 +1,28 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CreateAccount {
-    String login;
-    String password;
-    String confirmPassword;
+public class Account {
+    private String login;
+    private String password;
+    private String confirmPassword;
 
-    public CreateAccount(String login, String password, String confirmPassword) {
+    public Account(String login, String password, String confirmPassword) {
         checkAccount(login, password, confirmPassword);
         this.login = login;
         this.password = password;
         this.confirmPassword = confirmPassword;
+        System.out.println("Вы успешно зарегестрированы!");
     }
 
     private static Pattern allowedSymbols = Pattern.compile("^[a-zA-Z0-9_]{0,20}$");
 
-    public static void checkAccount(String login, String password, String confirmPassword) {
+    private void checkAccount(String login, String password, String confirmPassword) {
         checkLogin(login);
         checkPassword(password);
         confirmPassword(confirmPassword, password);
     }
 
-    private static void checkLogin(String login) {
+    private void checkLogin(String login) {
         Matcher checkLogin = allowedSymbols.matcher(login);
         if (!checkLogin.matches()) {
             throw new WrongLoginException("Login должен содержать только латинские буквы," +
@@ -29,7 +30,7 @@ public class CreateAccount {
         }
     }
 
-    private static void checkPassword(String password) {
+    private void checkPassword(String password) {
         Matcher checkPassword = allowedSymbols.matcher(password);
         if (!checkPassword.matches()) {
             throw new WrongPasswordException("Password должен содержать только латинские буквы," +
@@ -37,10 +38,8 @@ public class CreateAccount {
         }
     }
 
-    private static void confirmPassword(String confirmPassword, String password) {
-        if (confirmPassword.equals(password)) {
-            System.out.println("Вы успешно зарегестрированы!");
-        } else {
+    private void confirmPassword(String confirmPassword, String password) {
+        if (!confirmPassword.equals(password)) {
             throw new WrongPasswordException("Пароли не совпадают!");
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-
-
 public class Main {
     public static void main(String[] args) {
         try {
-            CreateAccount createAccount = new CreateAccount("login", "password", "password");
+            Account account = new Account("login", "password", "password");
         } catch (WrongLoginException ex) {
             System.out.println(ex.getMessage());
         } catch (WrongPasswordException ex) {
